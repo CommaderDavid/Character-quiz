2a318794635e98ad3fbd658be3523143f2156b7a
David Boedigheimer <stealth259@gmail.com>
2019-10-09 15:46:03 -0700

    Add start of jquery and background logic.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// back end
+var whichChar = function(fact1, fact2, fact3, fact4, fact5) {
+  if (true) {
+
+  }
+}
+// front end
+$(document).ready(function() {
+  $("#quiz").submit(function() {
+    var gender = 
+  });
+});
