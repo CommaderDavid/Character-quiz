2b1f3609abf89f231dd16146251bc39838a8b808
David Boedigheimer <stealth259@gmail.com>
2019-10-10 13:08:12 -0700

    Add function and submit button in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 // back end
-var whichChar = function(fact1, fact2, fact3, fact4, fact5) {
-  if (true) {
-
+var whichChar = function(gender, age, pet, classes, wand) {
+  if (gender === "male" && age > 11 && age < 20 && pet === "owl" && classes === "dark" && wand === "ash") {
+    return "Cedric Diggory"
+  } else if (gender === "female" && age > 25 && pet === "cat" && classes === "potion" && wand === "birch") {
+    return "Dolores Umbridge"
+  } else if (gender === "male" && age > 11 && age < 21 && classes === "dark" && wand === "cherry") {
+    return "Neville Longbottom"
+  } else if (age >= 11 && wand === "ebony") {
+    return "A brand new character"
+  } else if (age <= 10) {
+    return "(sorry your too young to go to hogwarts)"
+  } else {
+    return "a character not mentioned in Harry Potter"
   }
 }
 // front end
 $(document).ready(function() {
-  $("#quiz").submit(function() {
-    var gender = 
+  $("#quiz").submit(function(e) {
+    var gender = $("select#gender").val();
+    var age = parseInt($("input#age").val());
+    var pet = $("select#pet").val();
+    var classes = $("select#class").val();
+    var wand = $("select#wand").val();
+    console.log(gender, age, pet, classes, wand);
+    var result = whichChar(gender, age, pet, classes, wand);
+
+    $("#char").empty().append(result);
+    $("#hide").show();
+
+    e.preventDefault();
   });
 });
